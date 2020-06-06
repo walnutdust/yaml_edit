f876712ee4ff8e39c8614b0d6adbc6b5eb2fc9c5
Garett Tok <garettok@hotmail.com>
2020-06-06 20:06:00 +0800

    Cleanup

--- test/mod_utils.dart
+++ test/mod_utils.dart
@@ -8,18 +8,11 @@ void expectUnchangedYamlAfterLoading(String source) {
   expect(doc.toString(), equals(source));
 }
 
-void _expectModifiableYAMLEquals(dynamic modYAMLValue, dynamic value) {
-  if (modYAMLValue is Map && value is Map) {
-  } else if (modYAMLValue is List && value is List) {}
-
-  expect(modYAMLValue, equals(value));
-}
-
 /// Asserts that [builder] has the same internal value as [value].
 void expectYamlBuilderValue(YamlEditBuilder builder, dynamic value) {
   var builderValue = builder.getValueIn([]);
 
+  // Equals creates a matcher that uses the equality defined in builderValue, so we have
+  // to swap the expected/actual around.
   expect(value, equals(builderValue));
-
-  expect(builderValue.toString(), equals(value.toString()));
 }
